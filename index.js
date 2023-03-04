5093eb984103a5049ab9729a09459cbc08edd6a5
RudraTheBoy <126378886+RudraTheBoy@users.noreply.github.com>
2023-03-04 13:23:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+document.addEventListener('keydown', (event) => {
+    console.log(event.keyCode)
+    var key = event.keyCode;
+    if(key == 65){
+        var audio = new Audio("A.mp3");
+        audio.currentTime = 0;
+        audio.play();
+        document.getElementById('65').style.backgroundColor = "grey"
+        document.getElementById('65').style.boxShadow = "10px a0px 20px 10px 10px black"
+        setTimeout(function() {
+            document.getElementById('65').style.backgroundColor = "white"
+            document.getElementById('65').style.boxShadow = "none"
+        },200)
+}
+    if(key == 90){
+        var audio = new Audio("Z.mp3");
+        audio.currentTime = 0;
+        audio.play();
+        document.getElementById('90').style.backgroundColor = "grey"
+        document.getElementById('90').style.boxShadow = "10px a0px 20px 10px 10px black"
+        setTimeout(function() {
+            document.getElementById('90').style.backgroundColor = "black"
+            document.getElementById('90').style.boxShadow = "none"
+        },200)
+}
+    if(key == 83){
+        var audio = new Audio("S.mp3");
+        audio.currentTime = 0;
+        audio.play();
+        document.getElementById('83').style.backgroundColor = "grey"
+        document.getElementById('83').style.boxShadow = "10px a0px 20px 10px 10px black"
+        setTimeout(function() {
+            document.getElementById('83').style.backgroundColor = "white"
+            document.getElementById('83').style.boxShadow = "none"
+    },200)
+}
+    if(key == 88){
+        var audio = new Audio("X.mp3");
+        audio.currentTime = 0;
+        audio.play();
+        document.getElementById('88').style.backgroundColor = "grey"
+        document.getElementById('88').style.boxShadow = "10px a0px 20px 10px 10px black"
+        setTimeout(function() {
+            document.getElementById('88').style.backgroundColor = "black"
+            document.getElementById('88').style.boxShadow = "none"
+    },200)
+}
+if(key == 68){
+    var audio = new Audio("D.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('68').style.backgroundColor = "grey"
+    document.getElementById('68').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+        document.getElementById('68').style.backgroundColor = "white"
+        document.getElementById('68').style.boxShadow = "none"
+},200)
+}
+if(key == 70){
+    var audio = new Audio("F.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('70').style.backgroundColor = "grey"
+    document.getElementById('70').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+        document.getElementById('70').style.backgroundColor = "white"
+        document.getElementById('70').style.boxShadow = "none"
+},200)
+}
+    if(key == 67){
+        var audio = new Audio("C.mp3");
+        audio.currentTime = 0;
+        audio.play();
+        document.getElementById('67').style.backgroundColor = "grey"
+        document.getElementById('67').style.boxShadow = "10px a0px 20px 10px 10px black"
+        setTimeout(function() {
+           document.getElementById('67').style.backgroundColor = "black"
+            document.getElementById('67').style.boxShadow = "none"
+},200)
+}
+if(key == 71){
+    var audio = new Audio("G.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('71').style.backgroundColor = "grey"
+    document.getElementById('71').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+        document.getElementById('71').style.backgroundColor = "white"
+        document.getElementById('71').style.boxShadow = "none"
+},200)
+}
+if(key == 86){
+    var audio = new Audio("V.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('86').style.backgroundColor = "grey"
+    document.getElementById('86').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+       document.getElementById('86').style.backgroundColor = "black"
+        document.getElementById('86').style.boxShadow = "none"
+},200)
+}
+if(key == 72){
+    var audio = new Audio("H.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('72').style.backgroundColor = "grey"
+    document.getElementById('72').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+        document.getElementById('72').style.backgroundColor = "white"
+        document.getElementById('72').style.boxShadow = "none"
+},200)
+}
+if(key == 66){
+    var audio = new Audio("B.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('66').style.backgroundColor = "grey"
+    document.getElementById('66').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+       document.getElementById('66').style.backgroundColor = "black"
+        document.getElementById('66').style.boxShadow = "none"
+},200)
+}
+if(key == 74){
+    var audio = new Audio("J.mp3");
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById('74').style.backgroundColor = "grey"
+    document.getElementById('74').style.boxShadow = "10px a0px 20px 10px 10px black"
+    setTimeout(function() {
+        document.getElementById('74').style.backgroundColor = "white"
+        document.getElementById('74').style.boxShadow = "none"
+},200)
+}
+})
